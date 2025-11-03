4e7296369766f2a72939b2e5a160216662794f32
sixenn45 <bhelerpuyuh@gmail.com>
2025-11-03 22:41:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,55 @@ SESSION  = os.getenv('SESSION')
 GRUPS    = [g.strip() for g in os.getenv('GRUPS').split(',') if g.strip()]
 
 KATA = [
-    "⚡JOIN GRUP INFOSCAMMER @Info_Scammer_Shell2! 🔥",
-    "REKBER ON!! JOIN INFOSCAMMER @Info_Scammer_Shell2",    
-    "⚡OPEN PEMBELAJARAN TANAM SHELL @jktblackhat",
-    "⚡OPEN PEMBELAJARAN DORKING,HACKING,BUAT TOOLS HACK DENGAN AI PM @jktblackhat",
-    "⚡BIKIN TOOLS PHISING, HACKING DLL DENGAN AI pm @jktblackhat",
-    "⚡KAPOK DITIPU? JOIN @Info_Scammer_Shell2",
-    "⚡MAU BIKIN AUTO SEND KE GRUP OTOMATIS 24JAM? PM @jktblackhat",
-    "JOIN GRUP UNTUK UPDATE INFO SCAMMER"
+    """🌋 OPEN CLASS TANAM SHELL 
+   🔥 BELAJAR EXPLOIT
+   🛡️ BELAJAR BYPASS
+   💀 Payload 2025 (work 100%)
+   📤 BELAJAR BIKIN DORK
+   ⚡ PEMBELAJAR SAMPAI BISA
+   🕶️ PM @jktblackhat""",
+
+    """🚀 MAU AUTO SEND GRUP BUAT PROMO?
+   ⏰ BOT RUN 24JAM (AMAN)
+   🎲 10.000 variasi kata
+   🛡️ 0% flood wait
+   📊 Tested 120 hari
+   🎨 Support emoji + sticker
+   🔄 Auto update pesan
+PM @jktblackhat""",
+
+    """🛡️ JOIN GRUP ANTISCAMEMER
+   ⚠️ BIAR TRANSAKSI AMAN
+   📛 LIST SCAMMER
+   🔍 Cek NOREK SCAMMEMR
+   🏆 Rekber ON fee 2%
+   🍒@Info_Scammer_Shell2""",
+
+    """💎 REKBER PREMIUM 24 JAM
+   💸 Fee termurah 1.5%
+   ⚡ PASTINYA AMAN DAN NYAMAN
+   🤑 ALL PAYMENT
+   🛡️ FAST RESPON
+   📈 ANTI DRAMA
+   🎖️ Admin online 24/7
+PM @jktblackhat""",
+
+    """⚡ PRIVATE TOOLS 
+   📡 TOOLS BRUTE FORCE ALL CMS
+   🔍 GRABBER DOMAIN 
+   🕷️ AUTO UPLOAD SHELL WORDPRESS
+   💾 DIAJARIN CARA JALANIN TOOLS SAMPAI PAHAM
+   🥰 PM @jktblackhat""",
+
+    """🎣 OPEN PEMBUATAN PHISINK ALL SOSMED
+   🤖 TRUE LOG IN TELEGRAM KODE OTP ASLI
+   🌐 FACEBOK,INSTAGRAM,DLL DAN LIAT HASIL BISA LEWAT BOT TELE
+   🔐 Bypass 2FA 
+   📧 SCRIPT AMAN ANTI DETEK
+   🌍 50 template ready
+   🏆 99% success rate
+PM @jktblackhat"""
+
 ]
 
 client = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
@@ -26,7 +67,7 @@ print("JINX SPAM ORANG 24 JAM JALAN DI RAILWAY — NO CRASH!")
 with client:
     client.start()
     while True:
-        pesan = random.choice(KATA) + f"\n⏰ {time.strftime('%H:%M:%S')}"
+        pesan = random.choice(KATA) + "\n\n🔥JASEB BY @jktblackhat"
         for g in GRUPS:
             try:
                 client.send_message(g, pesan)
