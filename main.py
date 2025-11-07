6950f7bf7003a6c85bd6ced0e7b6a85da9f4a045
sixenn45 <bhelerpuyuh@gmail.com>
2025-11-08 01:28:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ async def list(event):
     await event.reply(txt)
 
 # TAMBAH PESAN
-@bot.on(events.NewMessage(pattern=r'/addpesan\s+(.+)', Re.S))
+@bot.on(events.NewMessage(pattern=r'/addpesan\s+(.+)', re.S))
 async def addpesan(event):
     pesan = event.pattern_match.group(1).strip()
     if pesan in data['pesan_list']:
